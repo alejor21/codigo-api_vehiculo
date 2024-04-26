b9dceabab09fbce2d82955adc3d05403993852e0
jhosept <jhosept.villota@campusucc.edu.co>
2024-04-26 09:54:01 -0500

    subiendo

---
+++ api_vehiculos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from api_vehiculos.views import vehiculo_api_view
+
+urlpatterns =[
+    path('crear_vehiculo',vehiculo_api_view.as_view()),
+    path('actualizar-vehiculo/<int:pkid>',vehiculo_api_view.as_view(),name='actualizar'),
+
+]
